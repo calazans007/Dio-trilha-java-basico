1b938d860e4a943c52f69e9fccd50eea36c6b0f2
tomaz <calazansr007@gmail.com>
2026-06-28 19:16:02 -0300

    variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,32 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        short idadeShort = 1;
+        System.out.println("Idade (short): " + idadeShort);
+
+        byte idadeByte = 127;
+        System.out.println("Idade (byte): " + idadeByte);
+
+        int idade = 127;
+        System.out.println("Idade: " + idade);
+
+        double altura = 1.75;
+        System.out.println("Altura: " + altura);
+
+        int anoFabricacao = 2020;
+        System.out.println("Ano de fabricação: " + anoFabricacao);
+
+        double salarioMensal = 2500.50;
+        System.out.println("Salário mensal: " + salarioMensal);
+
+        long populacaoMundial = 7800000000L;
+        System.out.println("População mundial: " + populacaoMundial);
+
+        boolean isJavaFun = true;
+        System.out.println("Java é divertido? " + isJavaFun);
+
+        float pi = 3.14f;
+        System.out.println("Valor de pi: " + pi);
     }
 }
