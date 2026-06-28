8cccf8b7dc785fa0251d0d0df64d7a8fa74e69f4
tomaz <calazansr007@gmail.com>
2026-06-28 19:27:12 -0300

    novo commit adicionando comentario

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -2,6 +2,26 @@ public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
+
+
+        // Declaração de variáveis vs constantes
+
+
+        // variáveis são declaradas com a palavra-chave var e podem ser alteradas posteriormente
+        var idade = 25; // Variável
+
+        int idade2 = 30; // Variável
+
+        idade = 26; // Alterando o valor da variável idade
+
+        // constantes são declaradas com a palavra-chave final e não podem ser alteradas após a atribuição inicial
+        final double PI = 3.14159; // Constante
+
+
+
+         // Tipos primitivos em Java
+        // exemplos de tipos primitivos: int, double, boolean, char, byte, short, long, float
+
         short idadeShort = 1;
         System.out.println("Idade (short): " + idadeShort);
 
