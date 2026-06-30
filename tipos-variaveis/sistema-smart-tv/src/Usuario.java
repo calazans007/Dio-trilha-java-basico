b4db8c1ef77d6fede0c7428f4ad1baf9b75f57d3
tomaz <calazansr007@gmail.com>
2026-06-29 21:09:32 -0300

    Metodos Smartv

---
+++ tipos-variaveis/sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,26 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("Tv ligada? " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo status -> Tv ligada? " + smartTv.ligada);
+
+        smartTv.aumentarVolume();
+        System.out.println("Novo volume: " + smartTv.volume);
+
+        smartTv.diminuirVolume();
+        System.out.println("Novo volume: " + smartTv.volume);
+
+        System.out.println("Canal atual: " + smartTv.canal);
+
+        smartTv.mudarCanal(13);
+        System.out.println("Novo canal: " + smartTv.canal);
+
+        smartTv.desligar();
+        System.out.println("Novo status -> Tv ligada? " + smartTv.ligada);
+    }
+}
